3147b1d74088ffd2bd43c98f05c0a61658c13a86
KimSeonjae <sun4035@inha.edu>
2025-06-05 15:22:11 +0900

    로그인 세션 추가

---
+++ LibraryProject/src/util/SessionContext.java
@@ -0,0 +1,27 @@
+package util;
+
+public class SessionContext {
+    private static SessionContext instance;
+    private String currentUserId;
+
+    private SessionContext() {}
+
+    public static SessionContext getInstance() {
+        if (instance == null) {
+            instance = new SessionContext();
+        }
+        return instance;
+    }
+
+    public void setCurrentUserId(String userId) {
+        this.currentUserId = userId;
+    }
+
+    public String getCurrentUserId() {
+        return currentUserId;
+    }
+
+    public void clear() {
+        currentUserId = null;
+    }
+}
